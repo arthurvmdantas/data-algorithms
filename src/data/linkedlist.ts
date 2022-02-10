8bad1da487fe37c18249f0499876956ba551ea59
Arthur V. M. Dantas <arthurvmdantas@gmail.com>
2022-02-10 08:11:03 -0300

    implemented methods to add values to the list (append and prepend) and iterator

---
+++ src/data/linkedlist.ts
@@ -0,0 +1,77 @@
+type LinkedListNode<T> = {
+  value: T;
+  next: LinkedListNode<T> | null;
+};
+
+export class LinkedList<T> {
+  #head: LinkedListNode<T> | null = null;
+  #size = 0;
+
+  constructor() {}
+
+  /**
+   * Add a node to the end of the list
+   * @param value
+   * @returns
+   */
+  append(value: T) {
+    if (!this.#head) return this.prepend(value);
+
+    const newNode: LinkedListNode<T> = { value, next: null };
+    let pointer = this.#head;
+
+    while (pointer.next) {
+      pointer = pointer.next;
+    }
+
+    pointer.next = newNode;
+    this.#size++;
+  }
+
+  /**
+   * Add a node to the beginning of the list
+   * @param value Value to be stored on the new node
+   */
+  prepend(value: T) {
+    const newHead: LinkedListNode<T> = {
+      value,
+      next: this.#size ? this.#head : null,
+    };
+
+    this.#head = newHead;
+    this.#size++;
+  }
+
+  *[Symbol.iterator]() {
+    let currentNode = this.#head;
+
+    while (currentNode) {
+      yield currentNode.value;
+      currentNode = currentNode.next;
+    }
+  }
+
+  // iterator
+  // [Symbol.iterator]() {
+  //   let currentNode = this.#head;
+
+  //   return {
+  //     next() {
+  //       if (currentNode) {
+  //         const value = currentNode.value;
+  //         currentNode = currentNode.next;
+
+  //         return {
+  //           value,
+  //           done: false,
+  //         };
+  //       }
+
+  //       return {
+  //         value: null,
+  //         done: true,
+  //       };
+  //     },
+  //   };
+  // }
+}
